deca26b7c6edf29778e3c56a7a10b6f43c8c6ab9
Ting-Ying Chu (UNIV AACHEN) <chu06@imec.be>
2024-09-03 14:11:53 +0200

    edit code for checking evaluation and statistic value for config

--- RadIal/3-Evaluation.py
+++ RadIal/3-Evaluation.py
@@ -4,17 +4,17 @@ import argparse
 import torch
 import random
 import numpy as np
-from model.FFTRadNet_ViT import FFTRadNet_ViT
+from model.FFTRadNet_ViT_ddp import FFTRadNet_ViT
 from model.FFTRadNet_ViT import FFTRadNet_ViT_ADC
 from dataset.dataset import RADIal
-from dataset.encoder import ra_encoder
-from dataset.dataloader import CreateDataLoaders
+from dataset.encoder_modi import ra_encoder
+from dataset.dataloader_ddp import CreateDataLoaders
 import pkbar
 import torch.nn.functional as F
-from utils.evaluation import run_FullEvaluation
+from utils.evaluation_ddp import run_FullEvaluation_
 import torch.nn as nn
 
-from dataset.matlab_dataset import MATLAB
+from dataset.matlab_dataset_ddp import MATLAB
 
 def main(config, checkpoint,difficult):
 
@@ -50,6 +50,7 @@ def main(config, checkpoint,difficult):
         #                     difficult=True,perform_FFT=config['data_mode'])
 
         dataset = MATLAB(root_dir = config['dataset']['root_dir'],
+                         folder_dir = config['dataset']['data_folder'],
                             statistics= config['dataset']['statistics'],
                             encoder=enc.encode)
     
@@ -69,8 +70,8 @@ def main(config, checkpoint,difficult):
                             statistics= config['dataset']['statistics'],
                             encoder=enc.encode,
                             difficult=True,perform_FFT='ADC')
-
-    train_loader, val_loader, test_loader = CreateDataLoaders(dataset,config['dataloader'],config['seed'])
+    batch_size = 4
+    train_loader, val_loader, test_loader = CreateDataLoaders(dataset, batch_size, config['dataloader'],config['seed'])
 
     print("Parameters: ",sum(p.numel() for p in net.parameters() if p.requires_grad))
 
@@ -82,8 +83,39 @@ def main(config, checkpoint,difficult):
     net.load_state_dict(dict['net_state_dict'])
 
     print('===========  Running the evaluation ==================:')
-    run_FullEvaluation(net,train_loader,enc,config=config) #test_loader
+    output_file = os.path.join('TFFTRadNet_detection_score.txt')
+    print("Saving scores to:", output_file)
+    with open(output_file, 'a') as f:
+        f.write('------- Train ------------\n')
+    result_train = run_FullEvaluation_(net,train_loader,enc,config=config) #test_loader
+    with open(output_file, 'a') as f:
+        f.write('------- Validation ------------\n')
+    result_val = run_FullEvaluation_(net,val_loader,enc,config=config) 
+    with open(output_file, 'a') as f:
+        f.write('------- Test ------------\n')
+    result_test = run_FullEvaluation_(net,test_loader,enc,config=config) 
 
+    # Transpose the data, excluding the IOU_threshold (index 0)
+    transposed_train_data = list(zip(*result_train))[1:]  # Skips the first element (IOU_threshold)
+    ransposed_val_data = list(zip(*result_val))[1:]  # Skips the first element (IOU_threshold)
+    ransposed_test_data = list(zip(*result_test))[1:]  # Skips the first element (IOU_threshold)
+
+    # Calculate the mean of each metric
+    means_train = [np.mean(metric) for metric in transposed_train_data]
+    means_val = [np.mean(metric) for metric in ransposed_val_data]
+    means_test = [np.mean(metric) for metric in ransposed_test_data]
+    
+    with open(output_file, 'a') as f:
+        f.write('------- Summary ------------\n')
+        f.write('------- Train ------------\n')
+        f.write('Means of Precision, Recall, F1_score, RangeError, AngleError:\n')
+        f.write(f"{means_train}\n")
+        f.write('------- Validation ------------\n')
+        f.write('Means of Precision, Recall, F1_score, RangeError, AngleError:\n')
+        f.write(f"{means_val}\n")
+        f.write('------- Test ------------\n')
+        f.write('Means of Precision, Recall, F1_score, RangeError, AngleError:\n')
+        f.write(f"{means_test}\n")
 
 
 
--- RadIal/dataset/print_dataset_statistics.py
+++ RadIal/dataset/print_dataset_statistics.py
@@ -1,14 +1,15 @@
 import sys
 sys.path.insert(0, '../')
 
-from dataset.dataset import RADIal
-from dataset.encoder import ra_encoder
-
+#from dataset.dataset import RADIal
+#from dataset.encoder import ra_encoder
+from dataset.encoder_modi import ra_encoder
+from dataset.matlab_dataset_ddp import MATLAB
 import numpy as np
 
 
 geometry = {    "ranges": [512,896,1],
-                "resolution": [0.201171875,0.2],
+                "resolution": [0.17,0.2], # meter
                 "size": 3}
 
 statistics = {  "input_mean":np.zeros(32),
@@ -20,19 +21,26 @@ enc = ra_encoder(geometry = geometry,
                     statistics = statistics,
                     regression_layer = 2)
 
-dataset = RADIal(root_dir = r"C:\Users\James-PC\James\data\RADIal",
-                       statistics=None,
-                       encoder=enc.encode,
-                       difficult=True,perform_FFT='Custom_RD')
+# dataset = RADIal(root_dir = r"C:\Users\James-PC\James\data\RADIal",
+#                        statistics=None,
+#                        encoder=enc.encode,
+#                        difficult=True,perform_FFT='Custom_RD')
+dataset = MATLAB(root_dir = '/imec/other/dl4ms/chu06/public/data/', #replace with the correct directory
+                 folder_dir = "simulation_sequential_rw_data_DDA_rawADC/",
+                 statistics= None, 
+                 encoder=enc.encode,perform_FFT='ADC')
 
 reg = []
 m=0
 s=0
 for i in range(len(dataset)):
     print(i,len(dataset))
-    radar_FFT, segmap,out_label,box_labels= dataset.__getitem__(i)
+    #radar_FFT, segmap,out_label,box_labels= dataset.__getitem__(i)
+    radar_FFT,out_label,box_labels= dataset.__getitem__(i)
+    print("radar_FFT: ", radar_FFT.shape)
 
-    data = np.reshape(radar_FFT,(512*256,32))
+    #data = np.reshape(radar_FFT,(512*256,32))
+    data = np.reshape(radar_FFT,(512*256,4)) # rx: 4, ADC don't separate img and real
 
     m += data.mean(axis=0)
     s += data.std(axis=0)
@@ -46,7 +54,15 @@ reg = np.concatenate(reg,axis=1)
 print('===  INPUT  ====')
 print('mean',m/len(dataset))
 print('std',s/len(dataset))
+# Print mean and std separated by commas
+print('mean:', ', '.join(map(str, m/len(dataset))))
+print('std:', ', '.join(map(str, s/len(dataset))))
 
 print('===  Regression  ====')
 print('mean',np.mean(reg,axis=1))
 print('std',np.std(reg,axis=1))
+# Print mean and std separated by commas
+print('mean:', ', '.join(map(str, np.mean(reg,axis=1))))
+print('std:', ', '.join(map(str, np.std(reg,axis=1))))
+# mean: 0.3676954382230018, 0.29876685086155963
+# std: 0.6753994169817015, 0.6905939539763425
